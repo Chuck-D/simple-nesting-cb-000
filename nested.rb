30a9ceec07fa9be9308c7b6169a40ddadedd84dd
Adrian Davis <chuck.d.kc@gmail.com>
2018-05-24 02:40:47 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -126,6 +126,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages]<<"Assembly"
+    x= programmer_hash[:dennis_ritchie][:languages]<<"Assembly"
+    x
+     
 
 end
